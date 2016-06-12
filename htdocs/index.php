7af3184aced4425f160e45a199cb991452b3f583
Dennis Otten <Dennis Otten>
2016-06-12 19:37:23 +0200

    First commit

---
+++ htdocs/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    <body>
+
+        <div class="container">
+            <h1><img src="img/logo.svg" class="logo" alt="do webdevelopment logo" title="do webdevelopment" /></h1>
+            <p>
+                <ul>
+                    <li>Dennis Otten</li>
+                    <li>dennis@dowebdevelopment.nl</li>
+                    <li>+31 6 55 735 115</li>
+                </ul>
+            </p>
+        </div>
+
+    </body>
+</html>
